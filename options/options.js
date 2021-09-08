f56b81e8fac6b57fcf0c5c700e207bf309bbf95a
Crowdbotics <team@crowdbotics.com>
2021-09-08 06:17:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "zack_30385Identifier",
+  url: "https://zack_30385Identifier.botics.co",
+  api: "https://zack_30385Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
